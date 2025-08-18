8db27973c267d1ec81d528cdd28234186aa24f5e
Pratap Rathi <prataprathi97@gmail.com>
2025-08-18 17:05:43 +0530

    chore: Added agents table, error & loading state and agents route

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,14 +1,11 @@
 "use client"
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
 
 const HomeView = () => {
-  const trpc = useTRPC();
-  const {data} = useQuery(trpc.hello.queryOptions({text: "welcome to my world !"}));
+
 
   return (
-    <div>{data?.greeting}</div>
+    <div>Home View</div>
   )
 }
 
