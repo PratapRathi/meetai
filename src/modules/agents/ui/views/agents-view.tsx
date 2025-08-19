ddc17978c80c0ca244b865d32ccf893c361ac56b
Pratap Rathi <prataprathi97@gmail.com>
2025-08-19 14:45:36 +0530

    chore: Added a Data-table for Agents list view

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -1,16 +1,25 @@
 "use client"
-import React from 'react'
-import { useTRPC } from '@/trpc/client'
-import { useSuspenseQuery } from '@tanstack/react-query';
+import { DataTable } from '@/modules/agents/ui/components/data-table';
+import { columns } from '@/modules/agents/ui/components/columns';
 import { LoadingState } from '@/components/loading-state';
+import { useSuspenseQuery } from '@tanstack/react-query';
 import { ErrorState } from '@/components/error-state';
+import { useTRPC } from '@/trpc/client'
+import { EmptyState } from '@/modules/agents/ui/components/empty-state';
+
+
 
 const AgentsView = () => {
     const trpc = useTRPC();
     const {data} = useSuspenseQuery(trpc.agents.getMany.queryOptions())
 
   return (
-    <div>{JSON.stringify(data, null, 2)}</div>
+    <div className='flex-1 px-4 pb-4 md:px-8 flex flex-col gap-y-4'>
+        <DataTable data={data} columns={columns}/>
+        {data.length === 0 && (
+            <EmptyState title='Create your first Agent' description='Create an agent to join your meetings. Each agent will follow your instructions and can interact with participants during the call.'/>
+        )}
+    </div>
   )
 }
 
