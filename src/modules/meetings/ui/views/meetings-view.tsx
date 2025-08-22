9924b8bddebd6a6ae4a14a179bd463ecead41c20
Pratap Rathi <prataprathi97@gmail.com>
2025-08-22 20:03:51 +0530

    chore: Added Meeetings data table

--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -1,29 +1,35 @@
-"use client"
-import { useTRPC } from "@/trpc/client"
+"use client";
+import { columns } from "@/modules/meetings/ui/components/columns";
+import { DataTable } from "@/components/data-table";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { LoadingState } from '@/components/loading-state';
-import { ErrorState } from '@/components/error-state';
-
+import { LoadingState } from "@/components/loading-state";
+import { ErrorState } from "@/components/error-state";
+import { useTRPC } from "@/trpc/client";
+import { EmptyState } from "@/components/empty-state";
 
 const MeetingsView = () => {
-    const trpc = useTRPC();
-    const {data} = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
 
   return (
-    <div>{JSON.stringify(data)}</div>
-  )
-}
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable data={data.items} columns={columns} />
+      {data.items.length === 0 && (
+        <EmptyState
+          title="Create your first Meeting"
+          description="Schedule a meeting to connect with others. Each meeting lets you collaborate, share ideas, and interact with participants on real time."
+        />
+      )}
+    </div>
+  );
+};
 
 export const MeetingsViewLoading = () => {
-    return (
-        <LoadingState title='Loading Meetings' description='This may take a few seconds'/>
-    )
-}
+  return <LoadingState title="Loading Meetings" description="This may take a few seconds" />;
+};
 
 export const MeetingsViewError = () => {
-    return (
-        <ErrorState title='Error loading Meetings' description='Something went wrong!'/>
-    )
-}
+  return <ErrorState title="Error loading Meetings" description="Something went wrong!" />;
+};
 
-export default MeetingsView
+export default MeetingsView;
