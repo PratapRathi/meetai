0be59d90a7c683061e1ca6eb1c645e6b64dd47fc
Pratap Rathi <prataprathi97@gmail.com>
2025-09-04 23:02:26 +0530

    fix: Updated the homepage routing to meetings

--- src/inggest/functions.ts
+++ src/inggest/functions.ts
@@ -36,7 +36,6 @@ export const meetingsProcessing = inngest.createFunction(
   { id: "meetings/processing" },
   { event: "meetings/processing" },
   async ({ event, step }) => {
-    // const response = await step.fetch(event.data.transcriptUrl);
     const response = await step.run("fetch-transcript", async () => {
       return fetch(event.data.transcriptUrl).then((res) => res.text());
     });
